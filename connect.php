990eafb6fa79ceadfc303f6fd278c2311f98f930
Alan <aln447@gmail.com>
2019-06-07 16:08:08 +0000

    Heroku setup

--- connect.php
+++ connect.php
@@ -1,14 +1,23 @@
 <?php 
 session_start();
 
+//local data
+// $servername = 'localhost';
+// $username = 'root';
+// $password = '';
+// $dbname = 'gaja';
 
-$servername = 'localhost';
-$username = 'root';
-$password = '';
-$dbname = 'gaja';
+/*
+Heroku Data: 
+mysql://ba7a0b3fdf9ae0:526d0ff2@us-cdbr-iron-east-02.cleardb.net/heroku_09d49274fea8a0d?reconnect=true
+*/
+$dbHost = 'us-cdbr-iron-east-02.cleardb.net';
+$dbName = 'heroku_09d49274fea8a0d';
+$dbUser = 'ba7a0b3fdf9ae0';
+$dbPasswd = '526d0ff';
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($dbHost, $dbUser, $dbPasswd, $dbName);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
