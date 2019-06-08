42f09ce373773878f163bbf992c283004b093107
Alan <aln447@gmail.com>
2019-06-08 12:57:14 +0000

    Hide comment section

--- post.php
+++ post.php
@@ -38,67 +38,67 @@ $category = $result->fetch_assoc()['name'];
 
                     <hr>
 
-                    <hr>
-                    <h2>Komentarze</h2>
-                    <div class="new-comment">
-                        <h3>Dodaj komentarz</h3>
-                        <form action="">
-                            <div class="form-group"><label for="nick">Twój nick</label><input type="text"
-                                    class="form-control" required></div>
-                            <div class="form-group"><label for="comment">Komentarz</label>
-                                <textarea name="comment" id="" rows="3" class="form-control" required></textarea>
-                            </div>
-                            <div class="form-group text-right">
-                                <button type="submit" class="btn btn-primary">Dodaj komentarz</button>
-                            </div>
-                        </form>
-                    </div>
-                    <hr>
-                    <div class="comment-list">
-                        <div class="comment">
-                            <p class="comment__content" style="font-style: itallic;">Lorem ipsum dolor sit amet
-                                consectetur adipisicing elit. Doloribus nobis cumque placeat ea delectus soluta, aliquam
-                                et omnis quis facere.</p>
-                            <p class="comment__data">Od <a href="#">User</a> | 23.11.19r.</p>
-                        </div>
-                        <div class="comment">
-                            <p class="comment__content" style="font-style: itallic;">Lorem ipsum dolor sit amet
-                                consectetur adipisicing elit. Doloribus nobis cumque placeat ea delectus soluta, aliquam
-                                et omnis quis facere.</p>
-                            <p class="comment__data">Od <a href="#">User</a> | 23.11.19r.</p>
-                        </div>
-                        <div class="comment">
-                            <p class="comment__content" style="font-style: itallic;">Lorem ipsum dolor sit amet
-                                consectetur adipisicing elit. Doloribus nobis cumque placeat ea delectus soluta, aliquam
-                                et omnis quis facere.</p>
-                            <p class="comment__data">Od <a href="#">User</a> | 23.11.19r.</p>
-                        </div>
-                        <div class="comment">
-                            <p class="comment__content" style="font-style: itallic;">Lorem, ipsum dolor.</p>
-                            <p class="comment__data">Od <a href="#">User</a> | 23.11.19r.</p>
-                        </div>
-                        <div class="comment">
-                            <p class="comment__content" style="font-style: itallic;">Lorem ipsum dolor sit amet
-                                consectetur adipisicing elit. Suscipit esse saepe id! Delectus modi nisi quia
-                                consequatur. Temporibus facere aliquam ipsa repellat animi ad perferendis! Et officiis
-                                enim veniam veritatis?.</p>
-                            <p class="comment__data">Od <a href="#">User</a> | 23.11.19r.</p>
-                        </div>
-                        <div class="comment">
-                            <p class="comment__content" style="font-style: itallic;">Lorem ipsum dolor sit amet
-                                consectetur adipisicing elit. Doloribus nobis cumque placeat ea delectus soluta, aliquam
-                                et omnis quis facere.</p>
-                            <p class="comment__data">Od <a href="#">User</a> | 23.11.19r.</p>
-                        </div>
-                    </div>
-                    <ul class="pagination pagination-warning">
-                        <li class="active page-item">
-                            <a href="javascript:void(0);" class="page-link">1</a>
-                        </li>
-                        <li class="page-item">
-                            <a href="javascript:void(0);" class="page-link">2</a>
-                        </li>
-                    </ul>
+                    <!--<hr>-->
+                    <!--<h2>Komentarze</h2>-->
+                    <!--<div class="new-comment">-->
+                    <!--    <h3>Dodaj komentarz</h3>-->
+                    <!--    <form action="">-->
+                    <!--        <div class="form-group"><label for="nick">Twój nick</label><input type="text"-->
+                    <!--                class="form-control" required></div>-->
+                    <!--        <div class="form-group"><label for="comment">Komentarz</label>-->
+                    <!--            <textarea name="comment" id="" rows="3" class="form-control" required></textarea>-->
+                    <!--        </div>-->
+                    <!--        <div class="form-group text-right">-->
+                    <!--            <button type="submit" class="btn btn-primary">Dodaj komentarz</button>-->
+                    <!--        </div>-->
+                    <!--    </form>-->
+                    <!--</div>-->
+                    <!--<hr>-->
+                    <!--<div class="comment-list">-->
+                    <!--    <div class="comment">-->
+                    <!--        <p class="comment__content" style="font-style: itallic;">Lorem ipsum dolor sit amet-->
+                    <!--            consectetur adipisicing elit. Doloribus nobis cumque placeat ea delectus soluta, aliquam-->
+                    <!--            et omnis quis facere.</p>-->
+                    <!--        <p class="comment__data">Od <a href="#">User</a> | 23.11.19r.</p>-->
+                    <!--    </div>-->
+                    <!--    <div class="comment">-->
+                    <!--        <p class="comment__content" style="font-style: itallic;">Lorem ipsum dolor sit amet-->
+                    <!--            consectetur adipisicing elit. Doloribus nobis cumque placeat ea delectus soluta, aliquam-->
+                    <!--            et omnis quis facere.</p>-->
+                    <!--        <p class="comment__data">Od <a href="#">User</a> | 23.11.19r.</p>-->
+                    <!--    </div>-->
+                    <!--    <div class="comment">-->
+                    <!--        <p class="comment__content" style="font-style: itallic;">Lorem ipsum dolor sit amet-->
+                    <!--            consectetur adipisicing elit. Doloribus nobis cumque placeat ea delectus soluta, aliquam-->
+                    <!--            et omnis quis facere.</p>-->
+                    <!--        <p class="comment__data">Od <a href="#">User</a> | 23.11.19r.</p>-->
+                    <!--    </div>-->
+                    <!--    <div class="comment">-->
+                    <!--        <p class="comment__content" style="font-style: itallic;">Lorem, ipsum dolor.</p>-->
+                    <!--        <p class="comment__data">Od <a href="#">User</a> | 23.11.19r.</p>-->
+                    <!--    </div>-->
+                    <!--    <div class="comment">-->
+                    <!--        <p class="comment__content" style="font-style: itallic;">Lorem ipsum dolor sit amet-->
+                    <!--            consectetur adipisicing elit. Suscipit esse saepe id! Delectus modi nisi quia-->
+                    <!--            consequatur. Temporibus facere aliquam ipsa repellat animi ad perferendis! Et officiis-->
+                    <!--            enim veniam veritatis?.</p>-->
+                    <!--        <p class="comment__data">Od <a href="#">User</a> | 23.11.19r.</p>-->
+                    <!--    </div>-->
+                    <!--    <div class="comment">-->
+                    <!--        <p class="comment__content" style="font-style: itallic;">Lorem ipsum dolor sit amet-->
+                    <!--            consectetur adipisicing elit. Doloribus nobis cumque placeat ea delectus soluta, aliquam-->
+                    <!--            et omnis quis facere.</p>-->
+                    <!--        <p class="comment__data">Od <a href="#">User</a> | 23.11.19r.</p>-->
+                    <!--    </div>-->
+                    <!--</div>-->
+                    <!--<ul class="pagination pagination-warning">-->
+                    <!--    <li class="active page-item">-->
+                    <!--        <a href="javascript:void(0);" class="page-link">1</a>-->
+                    <!--    </li>-->
+                    <!--    <li class="page-item">-->
+                    <!--        <a href="javascript:void(0);" class="page-link">2</a>-->
+                    <!--    </li>-->
+                    <!--</ul>-->
 
                 </section>
                 
